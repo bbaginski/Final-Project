c4a40fc5d1e733a84a822a12ff427631905dbc8a
tsimps19 <t.simps19@gmail.com>
2018-03-14 21:21:04 -0400

    scores

---
+++ scores/scores.js
@@ -0,0 +1,11 @@
+$http('https://api.fantasydata.net/v3/nba/scores/JSON/GamesByDate/2018-MAR-14', {
+    headers: {
+        'Ocp-Apim-Subscription-Key': '09283acc581042008ea48abcea846bf2'
+    }
+})
+
+// GET / v3 / nba / scores / JSON / GamesByDate / 2018 - MAR - 14 HTTP / 1.1
+// Host: api.fantasydata.net
+// Ocp - Apim - Subscription - Key: 09283 acc581042008ea48abcea846bf2
+// Cache - Control: no - cache
+// Postman - Token: fc5abce3 - b368 - 4 ce7 - 86 ad - b512a4e1a1d2
