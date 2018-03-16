73e7c8eea0348d05d902c76c6f4a47e47c88361b
bbaginski <excaliber2x@gmail.com>
2018-03-15 21:17:47 -0400

    made grid dynamicly generate

--- grid/grid-controller.js
+++ grid/grid-controller.js
@@ -4,6 +4,10 @@
         .module('app')
         .controller('GridController', function() {
             var vm = this; 
+            var savedSquare = [undefined, 'bob', undefined, undefined, 'brian'];
+            vm.showName = function(num){
+                return savedSquare[num];
+            }
             vm.awayTeamScore = [5,4,3,2,1,0,9,8,7,6];
             vm.homeTeamScore = [6,7,8,9,1,2,3,4,5,0];
             var grid = document.getElementById('Grid'); 
@@ -12,6 +16,7 @@
                 if (event.target.classList.contains('cell')){
                      console.log(event.target.id); 
                      event.target.classList.add("selected"); 
+                     event.target.innerHTML = "<span>Bob</span>"
                 }
               }
               
