cec7dd58f9f7eef28063ac0bc1a867038082d5a1
bbaginski <excaliber2x@gmail.com>
2018-03-15 20:39:56 -0400

    made grid dynamicly generate

--- grid/grid-controller.js
+++ grid/grid-controller.js
@@ -3,7 +3,27 @@
     angular
         .module('app')
         .controller('GridController', function() {
-            var vm = this;
+            var vm = this; 
+            vm.awayTeamScore = [5,4,3,2,1,0,9,8,7,6];
+            vm.homeTeamScore = [6,7,8,9,1,2,3,4,5,0];
+            var grid = document.getElementById('Grid'); 
+            
+            function cellClick(event) {
+                if (event.target.classList.contains('cell')){
+                     console.log(event.target.id); 
+                     event.target.classList.add("selected"); 
+                }
+              }
+              
+              grid.addEventListener('click', cellClick, false)
+
+              var cells = document.querySelectorAll('#Row1 .cell');
+                Array.prototype.forEach.call(cells, function(elements, index) {
+                    // conditional here.. access elements
+                });
+
+
+
         });
 
 
