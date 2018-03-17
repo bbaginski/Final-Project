c8c9e993615e356de7286db1a4a6a4babdb1e0ab
bbaginski <excaliber2x@gmail.com>
2018-03-17 16:56:55 -0400

    added scoreboard script and styles

--- grid/grid-controller.js
+++ grid/grid-controller.js
@@ -1,56 +1,61 @@
-
 (function() {
-    angular
-        .module('app')
-        .controller('GridController', function() {
-            var vm = this; 
-            var savedSquare = [undefined, 'bob', undefined, undefined, 'brian'];
-            vm.showName = function(num){
-                return savedSquare[num];
-            }
-            vm.isTaken = function(num){
-                if(savedSquare[num] !== undefined){
-                    return "taken";
-                }
+  angular.module("app").controller("GridController", function() {
+
+    var vm = this;
+    var savedSquare = [undefined, "bob", undefined, undefined, "brian"];
+    vm.showName = function(num) {
+      return savedSquare[num];
+    };
+    vm.isTaken = function(num) {
+      if (savedSquare[num] !== undefined) {
+        return "taken";
+      }
+    };
+    vm.isWinner = function(home, away, cellnum) {
                 
+      for (var i = 0; i < vm.awayTeamScore.length; i++) { 
+            if (home === (Math.abs(vm.homeTeamScore[i]) % 10) && away === (Math.abs(vm.awayTeamScore[i]) % 10) ) {
+               var sqName = savedSquare[cellnum];
+               vm.winList[i] = sqName;
+                console.log(savedSquare[cellnum]);
+                return "winner";
             }
-            vm.awayTeamBet = [5,4,3,2,1,0,9,8,7,6];
-            vm.homeTeamBet = [6,7,8,9,1,2,3,4,5,0];
-            vm.awayTeamScore = [1,0,0,0];
-            vm.homeTeamScore = [2,0,0,0];
+      }
+    };
+    vm.calcId = function(cIndex, pIndex) {        
+        var newID = cIndex + 1 + (pIndex * 10);
+        return newID;        
+      };
+ 
 
-             
-            var grid = document.getElementById('Grid'); 
-            var name = "Bob"
-            
-            function cellClick(event) {                
-                var cellId = event.target.id                       
-                var cellIdNum = cellId.replace(/\D/g,''); 
+    vm.homeTeamBet = [6, 7, 8, 9, 1, 2, 3, 4, 5, 0];
+    vm.awayTeamBet = [5, 4, 3, 2, 1, 0, 9, 8, 7, 6];
+    vm.homeTeamScore = [6, 10, 0, 0];
+    vm.awayTeamScore = [2, 12, 0, 0];
+    vm.winList = ["-----","-----","-----","-----"]
 
-                if (event.target.classList.contains('selected')){                     
-                    event.target.classList.remove("selected"); 
-                    event.target.innerHTML = "" ;                 
-                    savedSquare[cellIdNum] = undefined;      
-               }
-               else if (event.target.classList.contains('cell') && savedSquare[cellIdNum] === undefined){  
-                     event.target.classList.add("selected"); 
-                     event.target.innerHTML = name ;                
-                     savedSquare[cellIdNum] = name;
-                }
-                 
-              }
-              
-              grid.addEventListener('click', cellClick, false);
+    var grid = document.getElementById("Grid");
+    var name = "Bob";
 
-            //   var x = document.getElementsByClassName("cell")[0].getAttribute("col-num"); 
-            //   var y = document.getElementsByClassName("cell")[0].getAttribute("row-num");
-            //   if  (awayTeamScore[0] === x && homeTeamScore === y){
-            //         event.target.classList.add("winner"); 
-            //   }
+    function cellClick(event) {
+      var cellId = event.target.id;
+      var cellIdNum = cellId.replace(/\D/g, "");
 
+      if (event.target.classList.contains("selected")) {
+        event.target.classList.remove("selected");
+        event.target.innerHTML = "";
+        savedSquare[cellIdNum] = undefined;
 
+      } else if (event.target.classList.contains("winner")) {
+        console.log("boom");
 
-        });
+      } else if (event.target.classList.contains("cell") && savedSquare[cellIdNum] === undefined) {
+        event.target.classList.add("selected");
+        event.target.innerHTML = name;
+        savedSquare[cellIdNum] = name;
+      }
+    }
 
-
+    grid.addEventListener("click", cellClick, false);
+  });
 })();
