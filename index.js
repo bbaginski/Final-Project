51839bacc806fa6667b80e5b28c813eaf74cedb7
tsimps19 <t.simps19@gmail.com>
2018-03-20 21:29:12 -0400

    schedule

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 (function() {
 
     angular.module('app', ['ngRoute'])
-    .config( function($routeProvider){
-        $routeProvider
-            .when('/', {
-                template: '<registration></registration>'
-            })
-            .when('/grid', {
-                template:'<grid></grid>'
-            })
-    });    
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    template: '<registration></registration>'
+                })
+                .when('/grid', {
+                    template: '<grid></grid>'
+                })
+                .when('/schedule', {
+                    template: '<schedule></schedule>'
+                })
+        });
 })();
